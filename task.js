5970587200f08f13b756b55a430f4cfe67fe7b66
Shalaevskiy Nikolay <nshalaevskiy@intraservices.ru>
2024-06-14 15:48:11 +0300

    two commit

--- task.js
+++ task.js
@@ -3,10 +3,7 @@ const dropdownItems = document.querySelectorAll('.dropdown__item');
 
 // Функция для открытия/закрытия списка
 function toggleDropdown() {
-  // Проверяем, есть ли у списка класс 'dropdown__list_active'
-  const isActive = this.parentNode.classList.contains('dropdown__list_active');
-  
-  // Если список активен, закрываем его, иначе открываем
+   const isActive = this.parentNode.classList.contains('dropdown__list_active');  
   if (isActive) {
     closeDropdown();
   } else {
@@ -16,27 +13,27 @@ function toggleDropdown() {
 
 // Функция для открытия списка
 function openDropdown() {
-  // Находим родительский элемент списка
-  const list = this.parentNode;
-  
-  // Добавляем класс 'dropdown__list_active' к родительскому элементу списка
+  const list = document.querySelector('.dropdown__list');
   list.classList.add('dropdown__list_active');
-  
-  // Добавляем обработчик клика ко всем элементам списка
   dropdownItems.forEach(item => item.addEventListener('click', toggleDropdown));
 }
 
 // Функция для закрытия списка
 function closeDropdown() {
-  // Находим родительский элемент списка
-  const list = this.parentNode;
-  
-  // Удаляем класс 'dropdown__list_active' у родительского элемента списка
+  const list = document.querySelector('.dropdown__list');
   list.classList.remove('dropdown__list_active');
-  
-  // Удаляем обработчики кликов со всех элементов списка
   dropdownItems.forEach(item => item.removeEventListener('click', toggleDropdown));
+}
+
+// Функция для вывода названия
+function showTitle()  {
+  const title = document.querySelector('.dropdown__value');
+  const value = this.querySelector('.dropdown__link').textContent;
+  title.textContent = value;
 }
 
 // Добавляем обработчик клика к кнопке с выпадающим списком
 document.querySelector('.dropdown').addEventListener('click', openDropdown);
+
+const listLinks = document.querySelectorAll('.dropdown__link');
+listLinks.forEach(item => item.addEventListener('click', showTitle));
